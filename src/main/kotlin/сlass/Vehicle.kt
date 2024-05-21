37d3ca9624d9ffb2461cff40cb7b4982a181f351
Maxim Igumnov <161121113+MaximHoLyMaxim@users.noreply.github.com>
2024-05-21 23:52:39 +0300

    Vehicle.kt

---
+++ src/main/kotlin/сlass/Vehicle.kt
@@ -0,0 +1,15 @@
+package class
+
+open val speed = ""
+open val color = ""
+open val countOfWheels = ""
+open val nomer = ""
+open val firma = ""
+
+abstract fun makeSound()
+
+open fun displayinfo() {
+    println("Характеристики: /n")
+    println("Скорость: $speed Цвет: $color Количество колёс: $countOfWheels")
+    println("Фирма: $firma,Номер: $nomer, Скорость: $speed, Цвет: $color")
+}
